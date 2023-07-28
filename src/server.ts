fe581ba56977b145349e5c0cd8eb7ac35f36f961
Pablo Silva <pablo@buybye.com.br>
2023-07-28 07:16:41 -0300

    chore: configured eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 app.listen({
     port: env.PORT,
     host: '0.0.0.0' // listen on all interfaces
 }).then(() => {
-    console.log(`Server running on ${env.PORT}`);
+    console.log(`Server running on ${env.PORT}`)
 })
