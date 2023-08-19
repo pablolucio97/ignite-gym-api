46af32928817ea8caa6a89d092d8b6d4d044096d
Pablo Silva <pablo@buybye.com.br>
2023-08-19 12:11:37 -0300

    refactor: refactored tests to use beforeEach

--- src/useCases/authenticateUser/authenticateUser.spec.ts
+++ src/useCases/authenticateUser/authenticateUser.spec.ts
@@ -1,15 +1,20 @@
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
 import { hash } from 'bcryptjs';
-import { expect, it, test } from 'vitest';
+import { beforeEach, expect, it, test } from 'vitest';
 import { AuthenticateUserUseCase } from './authenticateUser';
 import { AppError } from '@/errors/AppError';
 
 test('AuthenticateUserUseCase', () => {
+
+    let inMemoryUsersRepository: InMemoryUsersRepository
+    let authenticateUserUseCase: AuthenticateUserUseCase
+
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository()
+        authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
+    })
+
     it('should be able to authenticate user', async () => {
-
-        const inMemoryUsersRepository = new InMemoryUsersRepository()
-        const authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
-
         const user = await inMemoryUsersRepository.create({
             name: 'john',
             email: 'john@example.com',
@@ -18,23 +23,16 @@ test('AuthenticateUserUseCase', () => {
             checkins: undefined,
             created_at: new Date(),
         })
-
         const { name } = await authenticateUserUseCase.execute({
             email: user.email,
             password: '123456'
         })
-
         expect(name).toBe('john-wrong')
-
     })
 
-    
+
     it('should not be able to authenticate with wrong email', async () => {
-        const inMemoryUsersRepository = new InMemoryUsersRepository()
-        const authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
-
         const email = 'johndoe-testr2@gmail.com'
-
         expect(async () => {
             return await authenticateUserUseCase.execute({
                 email,
@@ -44,11 +42,7 @@ test('AuthenticateUserUseCase', () => {
     })
 
     it('should not be able to authenticate with wrong password', async () => {
-        const inMemoryUsersRepository = new InMemoryUsersRepository()
-        const authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
-
         const email = 'johndoe-testr2@gmail.com'
-
         expect(async () => {
             return await authenticateUserUseCase.execute({
                 email,
--- src/useCases/registerUser/registerUser.spec.ts
+++ src/useCases/registerUser/registerUser.spec.ts
@@ -1,15 +1,20 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { RegisterUseCase } from './registerUser'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AppError } from '@/errors/AppError'
 
 describe('Register Use Case', () => {
+
+    let usersRepository: InMemoryUsersRepository
+    let registerUseCase: RegisterUseCase
+
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        registerUseCase = new RegisterUseCase(usersRepository)
+    })
+
     it('should hash user password correctly on registration', async () => {
-
-        const usersRepository = new InMemoryUsersRepository()
-        const registerUseCase = new RegisterUseCase(usersRepository)
-
         const { newUser } = await registerUseCase.execute({
             email: 'johndoe2@gmail.com',
             name: 'johndoe',
@@ -24,17 +29,12 @@ describe('Register Use Case', () => {
     })
 
     it('should not be able to register two users with same email', async () => {
-        const usersRepository = new InMemoryUsersRepository()
-        const registerUseCase = new RegisterUseCase(usersRepository)
-
         const email = 'johndoe-test@gmail.com'
-
         await registerUseCase.execute({
             email,
             name: 'johndoe',
             password: 'abc123'
         })
-
         expect(async () => {
             return await registerUseCase.execute({
                 email,
@@ -45,15 +45,11 @@ describe('Register Use Case', () => {
     })
 
     it('should be able to register an user', async () => {
-        const usersRepository = new InMemoryUsersRepository()
-        const registerUseCase = new RegisterUseCase(usersRepository)
-
         const { newUser } = await registerUseCase.execute({
             email: 'johndoe2@gmail.com',
             name: 'johndoe',
             password: 'abc123'
         })
-
         expect(newUser.id).toEqual(expect.any(String))
     })
 
