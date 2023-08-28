2d8ac7f38b5c0b5c8f5e06bc687f3c04b6a73aac
Pablo Silva <pablo@buybye.com.br>
2023-08-28 06:31:54 -0300

    feat: added search gyms by query use case (unsolved)

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -3,28 +3,34 @@ import { randomUUID } from "crypto"
 import { GymsRepository } from "../interfaces/prisma-gyms-repository"
 
 export class InMemoryGymsRepository implements GymsRepository {
-    public items: Gym[] = []
-    
-   async create(gymData: Prisma.GymCreateInput){
-      const gym = {
-        id: gymData.id ?? randomUUID(),
-        title: gymData.title,
-        description: gymData.description ?? null,
-        phone: gymData.phone ?? null,
-        latitude: new Prisma.Decimal(gymData.latitude.toString()),
-        longitude: new Prisma.Decimal(gymData.longitude.toString()),
-        created_at: new Date()
-      }
-      this.items.push(gym)
-      return gym
+  public items: Gym[] = []
+
+  async create(gymData: Prisma.GymCreateInput) {
+    const gym = {
+      id: gymData.id ?? randomUUID(),
+      title: gymData.title,
+      description: gymData.description ?? null,
+      phone: gymData.phone ?? null,
+      latitude: new Prisma.Decimal(gymData.latitude.toString()),
+      longitude: new Prisma.Decimal(gymData.longitude.toString()),
+      created_at: new Date()
     }
+    this.items.push(gym)
+    return gym
+  }
 
 
-    async findById(id: string) {
-        const gym = this.items.find((item) => item.id === id)
-        if (!gym) {
-            return null
-        }
-        return gym
+  async findById(id: string) {
+    const gym = this.items.find((item) => item.id === id)
+    if (!gym) {
+      return null
     }
+    return gym
+  }
+
+  async searchMany(searchTerm: string, page: number) {
+    return this.items
+      .filter(item => item.title.toLowerCase() === searchTerm.toLowerCase())
+      .slice((page - 1) * 20, page * 20)
+  }
 }
