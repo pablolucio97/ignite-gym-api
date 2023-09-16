14dbd298e0f682b8dcca95673749653d8e0be3b0
Pablo Silva <pablolucio_@hotmail.com>
2023-09-16 09:05:02 -0300

    chore: configured end to end tests environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import {defineConfig} from 'vitest/config'
 import tsconfigpaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [tsconfigpaths()]
+    plugins: [tsconfigpaths()],
+    test:{
+        //execute all tests that are inside controllers folder using the envinronment
+        //vitest-environment-prisma
+        environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+        dir: 'src'
+    }
 })
